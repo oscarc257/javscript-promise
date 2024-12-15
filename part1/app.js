818da49c57a562764024e6bea3b3d420272b2837
oscar <oscar.carrillo758@gmail.com>
2024-12-14 20:42:43 -0800

    first part

---
+++ part1/app.js
@@ -0,0 +1,22 @@
+let favNumber = 18;
+let baseURL = "http://numbersapi.com";
+
+// 1.
+$.getJSON(`${baseURL}/${favNumber}?json`).then(data => {
+  console.log(data);
+});
+
+// 2.
+let favNumbers = [6, 12, 30];
+$.getJSON(`${baseURL}/${favNumbers}?json`).then(data => {
+  console.log(data);
+});
+
+// 3.
+Promise.all(
+  Array.from({ length: 4 }, () => {
+    return $.getJSON(`${baseURL}/${favNumber}?json`);
+  })
+).then(facts => {
+  facts.forEach(data => $("body").append(`<p>${data.text}</p>`));
+});
